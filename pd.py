cdbce15dd98608cb4b03fdf9cd9e70bd872c4506
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2023-12-17 16:35:49 +0100

    Add check for identical names and contents, task 2 complete

--- pd.py
+++ pd.py
@@ -1,15 +1,50 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import webbrowser
+from pathlib import Path
+from collections import Counter
+import os
 
-auteurs = ['Vincent', 'Lisa', 'David']
+
+p = Path(input("Geef een filepath op: "))
+
+auteurs = [entry.name for entry in p.iterdir() if entry.is_dir()]
+print(auteurs)
 
 alias_mapping = {auteur: f'Auteur{i+1}' for i, auteur in enumerate(auteurs)}
 
 opmerkingen_matrix = {alias_mapping[auteur]: {alias_mapping[andere_auteur]: [] for andere_auteur in auteurs if andere_auteur != auteur} for auteur in auteurs}
 
-opmerkingen_matrix['Auteur1']['Auteur2'] = ['test']
-opmerkingen_matrix['Auteur2']['Auteur3'] = ['test2']
-opmerkingen_matrix['Auteur3']['Auteur3'] = ['test3']
+filepath = []
+for auteur in auteurs:
+    directory_path = p / auteur
+    file_paths = directory_path.glob("*.py")
+    for file_path in file_paths:
+        filepath.append(file_path)
+
+print(filepath)
+counter = 0
+
+for file_path in filepath:
+    counter += 1
+    selected_files = [f for f in filepath if f.name == file_path.name]
+    if len(selected_files) >= 2:
+        contents = {}
+        for f in selected_files:
+            with open(f, 'r', encoding='utf-8') as file:
+                file_content = file.read()
+                contents[f] = file_content
+        print(contents)      
+              
+        same_text = [key for key, value in contents.items() if value == contents[file_path]]
+        print(same_text)
+        
+        for item in same_text:
+            if str(item) != str(file_path):
+                print(file_path)
+                for aut in auteurs:
+                    if str(item).find(aut) > 0:
+                        file_name = os.path.basename(file_path)
+                        opmerkingen_matrix['Auteur' + str(counter)][alias_mapping[aut]] = [f'identieke file {file_name}']
 
 print(opmerkingen_matrix)
 
